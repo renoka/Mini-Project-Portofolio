bd9500dcd4645b3a8c2419ffb2ecad4588a58179
Renoka Tresna Pramuda <samsungrenoka@gmail.com>
2025-03-12 19:19:03 +0700

    configurasi awal mini project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-// js
+
+// function replacename(){
+// let nama = prompt('Halo, siapa nama anda','')
+
+// document.getElementById("nama").innerHTML = nama
+// }
+// replacename()
+
+// document.getElementById('klik').addEventListener('click', function(){replacename()})
+
